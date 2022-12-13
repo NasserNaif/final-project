f521856bf3e4c2c80ed3cd11446ae71dd66c09a6
Nasser <nmaif7788@hotmail.com>
2022-12-13 17:04:27 +0300

    gg

--- client/my-app/src/Pages/HomePage.tsx
+++ client/my-app/src/Pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { VStack } from "@chakra-ui/react";
 import React from "react";
 
 function HomePage() {
-  return <VStack h={"100vh"} bg="#e6e3e4"></VStack>;
+  return <VStack h={"90vh"} bg="#e6e3e4"></VStack>;
 }
 
 export default HomePage;
--- client/my-app/src/Pages/RequstCard.tsx
+++ client/my-app/src/Pages/RequstCard.tsx
@@ -12,12 +12,8 @@ import {
 import { DownloadIcon } from "@chakra-ui/icons";
 
 export default function form() {
-  
-
-
   return (
     <Center py="10px" mt="20px">
-        
       <Box
         maxW={"745px"}
         w={"full"}
@@ -27,47 +23,68 @@ export default function form() {
         p={6}
         overflow={"hidden"}
       >
-    <VStack alignItems={'center'}>
-         <Heading 
+        <VStack alignItems={"center"}>
+          <Heading
             fontWeight={300}
-            fontSize={{ base: '2xl', sm: '4xl', md: '4xl' }}
-            lineHeight={'110%'} color={'#120f2d'}>Company Name</Heading>
-    </VStack>
-    <VStack align={'start'}>
-        <HStack align={'start'} mt={6} p={3} border={'1px'} w={'100%'} borderColor={'#000'}
-        rounded={'2xl'}
-        >
-           
-            <Text>has description has description has description has description 
-            has description has description has description has description
-            has description has description has description has description
+            fontSize={{ base: "2xl", sm: "4xl", md: "4xl" }}
+            lineHeight={"110%"}
+            color={"#120f2d"}
+          >
+            Company Name
+          </Heading>
+        </VStack>
+        <VStack align={"start"}>
+          <HStack
+            align={"start"}
+            mt={6}
+            p={3}
+            border={"1px"}
+            w={"100%"}
+            borderColor={"#000"}
+            rounded={"2xl"}
+          >
+            <Text>
+              has description has description has description has description
+              has description has description has description has description
+              has description has description has description has description
             </Text>
-        </HStack>
-       <Text></Text>
-        <Button w={"250px"} bg={'#000'}color={'#fff'}
-        _hover={{
-            bg: 'gray.700',
-          }}  
-      
-        leftIcon={<DownloadIcon/>} >Download</Button>
-        
-        <HStack p={12} w={"100%"}   justifyContent={'center'}>
-            <Button bg={'#000'}color={'#fff'}
-        _hover={{
-            bg: 'gray.700',
-          }}  >Accept</Button>
+          </HStack>
+          <Text></Text>
+          <Button
+            w={"250px"}
+            bg={"#000"}
+            color={"#fff"}
+            _hover={{
+              bg: "gray.700",
+            }}
+            leftIcon={<DownloadIcon />}
+          >
+            Download
+          </Button>
 
-            <Button bg={'#000'}color={'#fff'}
-        _hover={{
-            bg: 'gray.700',
-          }}  >Reject</Button>
+          <HStack p={12} w={"100%"} justifyContent={"center"}>
+            <Button
+              bg={"#000"}
+              color={"#fff"}
+              _hover={{
+                bg: "gray.700",
+              }}
+            >
+              Accept
+            </Button>
 
-        </HStack>
-      
+            <Button
+              bg={"#000"}
+              color={"#fff"}
+              _hover={{
+                bg: "gray.700",
+              }}
+            >
+              Reject
+            </Button>
+          </HStack>
         </VStack>
       </Box>
-
-      
     </Center>
   );
 }
